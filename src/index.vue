aacdaa7ba0f3f0f660c803d2037bc3caa769d6c2
workkk <gentlebob1998@icloud.com>
2021-05-29 13:18:00 +0800

    vue hot reload api

---
+++ src/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ title }} + 11</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld3',
+  data () {
+    return {
+      title: 'Hello World1!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,33 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueloaderPlugin = require('vue-loader-plugin');
+
 module.exports = {
 	mode: 'development',
-	entry: './index.js',
+	entry: {
+		index: './index.js',
+		vueDemo: './vueDemo.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				use: 'vue-loader'
+			}
+		]
+	},
 	output: {
-		filename: 'bundle.js',
-		path: path.join(__dirname, '/')
+		filename: '[name]-bundle.js',
+		path: path.join(__dirname, 'dist')
 	},
 	devServer: {
-		hot: true
-	}
+		contentBase: './dist'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './index.html'
+		}),
+		new VueloaderPlugin()
+	]
 }
